925ddbd2e0b89012a575cd9b9f00df3663bbd4f7
Lamaw <amahouin@free.fr>
2016-06-07 17:15:23 +1200

    Comments added to lemonde_extractor.py

--- lemonde_extractor.py
+++ lemonde_extractor.py
@@ -67,36 +67,46 @@ class LeMonde_Extractor(ISite_Extractor):
         return text
 
 class LeMondeHTMLParser(HTMLParser):
+    """
+    Class implementating some methods of the HTMLParser pytho lib, in order to acquire specific data for Lemonde website
+    """
 
     def __init__(self):
-        HTMLParser.__init__(self)
-        self.links = list()
-        self.article_section = False
-        self.article_body = False
-        self.div_open_in_article_body = 0
-        self.article_data = ""
+        HTMLParser.__init__(self) # Parents constructor
+
+        self.links = list() # The list of links from the news feed
+        self.article_section = False # Flag for news feed parsing
+        self.article_body = False # Flag for article text acquisition
+        self.div_open_in_article_body = 0 # Number of open inside the main article div
+        self.article_data = "" # store the text from the article
 
     def handle_starttag(self, tag, attrs):
+        """
+        Method that manage tag opening in the HTML source code, to retrieve article content
+        """
         try:
-            if tag == "article":
+            if tag == "article": # Set flag for news feed parsing to true
                 for name, value in attrs:
                     if name == 'class' and 'grid_12 alpha enrichi' in value:
                         self.article_section = True
-            elif tag == "a" and self.article_section == True:
+            elif tag == "a" and self.article_section == True: # get a link from the news feed
                 for name, value in attrs:
                     if name == "href":
                         if value not in self.links:
                             self.links.append(value)
-            elif tag == "div" and not self.article_body:
+            elif tag == "div" and not self.article_body: # Set flag from article body to true
                 for name, value in attrs:
                     if name == 'id' and value == 'articleBody':
                         self.article_body = True
-            elif tag == 'div' and self.article_body:
+            elif tag == 'div' and self.article_body: # Increment number of open div in the main div of article (used to determine when the main article div is closed)
                 self.div_open_in_article_body += 1
         except:
             pass
 
     def handle_endtag(self, tag):
+        """
+        Method that Manage tag ending, in order to determine when parsing get out of appropriate sections
+        """
         try:
             if tag == "article":
                 self.article_section = False
@@ -109,5 +119,8 @@ class LeMondeHTMLParser(HTMLParser):
             pass
 
     def handle_data(self, data):
+        """
+        Store data when in right section of parsing
+        """
         if self.article_body:
             self.article_data += data
